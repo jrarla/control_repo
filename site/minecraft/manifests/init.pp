14f9bf128f544b472c83d7435d277b9d13a3203a
jrarla <49324293+jrarla@users.noreply.github.com>
2019-04-12 17:03:50 -0500

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -21,11 +21,11 @@ class minecraft (
     ensure => file,
     content => epp('minecraft/minecraft.service',{
       install_dir => $install_dir,
-    }),
+    })
   }
   service {'minecraft':
     ensure => running,
     enable => true,
-    require => [Package['java',File["${install_dir}/eula.txt"],File['/etc/systemd/system/minecraft.service']],
+    require => [Package['java'],File["${install_dir}/eula.txt"],File['/etc/systemd/system/minecraft.service']],
   }
 }
